7e45ba3172bfb57aa0e6488296ca30d5d08feabc
Geovani Benita <carlo.geovani@gmail.com>
2020-03-24 14:14:11 -0600

    E3 A6

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -3,6 +3,7 @@ import {Routes, RouterModule} from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ListComponent } from '../List/list.component';
 import { TableComponent } from '../table/table.component'
+import { PageNotFoundComponent } from '../page-not-found/page-not-found.component';
 
 const routes : Routes = [
 {
@@ -12,6 +13,10 @@ const routes : Routes = [
 {
   path :'table',
   component : TableComponent
+},
+{
+path : "**",
+component : PageNotFoundComponent
 }
 ];
 
