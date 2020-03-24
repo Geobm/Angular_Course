c922c5664be3ce7fe561c751eec0cd3747429861
Geovani Benita <carlo.geovani@gmail.com>
2020-03-23 21:02:46 -0600

    E2 A3

---
+++ src/app/data.ts
@@ -0,0 +1,44 @@
+import {Car} from './model'
+
+export const CARS: Car[] = [
+{
+  brand: 'Ford',
+  sub_brand: 'Mustang',
+  model : 2018,
+  description: 'Ford Mustang 2018 automatic',
+  registration_date : new Date('2020-03-08')},
+
+{
+  brand: "Audi",
+  sub_brand: "R8",
+  model : 2020,
+  description: "Audi R8 2020 standard",
+  registration_date : new Date("2020-03-08")},
+
+{
+
+  brand: "BMW",
+  sub_brand: "i250",
+  model : 2019,
+  description: "BMW i2150 2019 Automatic",
+  registration_date : new Date("2020-03-08")},
+
+{
+
+  brand: "Mercedes",
+  sub_brand: "AMG gtr",
+  model : 2020,
+  description: "Mercedes AMG gtr standard ",
+  registration_date : new Date("2020-03-08")},
+
+
+{
+
+  brand: "Lamborgini",
+  sub_brand: "Aventator",
+  model : 2019,
+  description: "Lamborgini Aventator 2019 standard",
+  registration_date : new Date("2020-03-08")},
+
+
+]
---
+++ src/app/model.ts
@@ -0,0 +1,8 @@
+export interface Car {
+
+  brand : string ;
+  sub_brand : string;
+  registration_date : Date;
+  description : string;
+  model : number;
+}
