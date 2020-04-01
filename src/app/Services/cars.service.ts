7cfd461147a6733a5fb17c811096c5a4fb2f8829
Geovani Benita <36940541+Geobm@users.noreply.github.com>
2020-04-01 15:58:14 -0600

    E7 A19

--- src/app/Services/cars.service.ts
+++ src/app/Services/cars.service.ts
@@ -1,32 +1,62 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import {Car} from '../model'
-import { Observable } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Car } from "../model";
+import { Observable } from "rxjs";
+import { catchError, tap } from "rxjs/operators";
+import { MessageService } from "../Services/message.service";
 
 @Injectable({
-  providedIn : 'root'
+  providedIn: "root"
 })
 export class CarsService {
-  private CarURL = 'https://catalogo-autos.herokuapp.com/api/autos/limit/300';
-  private CarActionURL = 'https://catalogo-autos.herokuapp.com/api/autos';
+  private CarURL = "https://catalogo-autos.herokuapp.com/api/autos/limit/300";
+  private CarActionURL = "https://catalogo-autos.herokuapp.com/api/autos";
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private messagesService: MessageService
+  ) {}
 
- getCars(): Observable<any>{
+  getCars(): Observable<any> {
+    return this.http.get<any>(this.CarURL).pipe(
+      catchError(this.handleError<any>("getCars")),
+      tap(() => this.messagesService.add("Obtained Cars"))
+    );
+  }
 
-   return this.http.get<any>(this.CarURL);
-   
- }
+  updateCars(car: Car): Observable<any> {
+    return this.http.put<any>(`${this.CarActionURL}/${car._id}`, car).pipe(
+      catchError(this.handleError<any>("updateCars")),
+      tap(result =>
+        this.messagesService.add(
+          `Car updated correctly with ID :${result.data._id}`
+        )
+      )
+    );
+  }
 
- updateCars(car:Car): Observable <any>{
-   return this.http.put<any>(`${this.CarActionURL}/${car._id}`, car);
- }
+  addCars(car: Car): Observable<any> {
+    return this.http.post<any>(this.CarActionURL, car).pipe(
+      catchError(this.handleError<any>("addCars")),
+      tap(result =>
+        this.messagesService.add(
+          `Car added correctly with ID :${result.data._id}`
+        )
+      )
+    );
+  }
 
- addCars(car : Car): Observable<any>{
-   return this.http.post<any>(this.CarActionURL,car);
- }
+  deleteCars(car: Car): Observable<any> {
+    return this.http.delete<any>(`${this.CarActionURL}/${car._id}`).pipe(
+      catchError(this.handleError<any>("deleteCars")),
+      tap(() => this.messagesService.add("Car deleted correctly"))
+    );
+  }
 
- deleteCars(car : Car): Observable<any>{
-   return this.http.delete<any>(`${this.CarActionURL}/${car._id}`);
- }
+  private handleError<T>(operation = "operation", result?: T) {
+    return (error: any): Observable<T> => {
+      this.messagesService.add(`${operation} failed : ${error.message}`);
+      return of(result as T);
+    };
+  }
 }
